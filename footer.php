31b95c1f2ebf780e8e8027f36e8086dc520552a5
Simon Whelan <simon.whelan.email@bearingpoint.com>
2018-12-18 20:56:33 +0000

    Init commit for mill quarter wordpress theme

---
+++ footer.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link
+ *
+ * @package Clearsoft
+ * @subpackage RedmondsFarm
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<!-- FOOTER -->
+<footer id="footer">
+    <div class="copyright">
+        <div class="container hidden-sm-down">
+            <div class="float-right">
+                <a href="https://www.sherryfitz.ie/branch/gorey" target="_blank">Sherry FitzGerald O'Leary Kinsella</a> | tel : <a href="0539430088" type="tel">053 9430088</a>
+            </div>
+            <div class="float-left">
+                Website By <a href="https://www.clearsoft.ie" target="_blank">ClearSoft</a>
+            </div>
+        </div>
+        <div class="container hidden-sm-up">
+            <div class="text-center">
+                <a href="https://www.sherryfitz.ie/branch/gorey" target="_blank">Sherry FitzGerald O'Leary Kinsella</a> | tel : <a href="0539430088" type="tel">053 9430088</a>
+            </div>
+            <div class="text-center">
+                Website By <a href="https://www.clearsoft.ie" target="_blank">ClearSoft</a>
+            </div>
+        </div>
+    </div>
+</footer>			<!-- /FOOTER -->
+
+</div>
+<!-- /wrapper -->
+
+
+<!-- SCROLL TO TOP -->
+<a href="#" id="toTop"></a>
+
+
+<!-- PRELOADER -->
+<div id="preloader">
+    <div class="inner">
+        <span class="loader"></span>
+    </div>
+</div><!-- /PRELOADER -->
+
+
+<!-- JAVASCRIPT FILES -->
+<script>var plugin_path = '<?php echo get_template_directory_uri() ;?>/assets/plugins/';</script>
+<script src="<?php echo get_template_directory_uri() ;?>/assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+
+<!-- SCRIPTS -->
+<script src="<?php echo get_template_directory_uri() ;?>/assets/js/scripts.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
